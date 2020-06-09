d5928f4700119c6eaddae975f3d7f40059a5053b
Adith A Danthi <adith.danthi@gmail.com>
2020-06-09 14:05:47 +0530

    Basic CNN

---
+++ cnn_mnist.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+
+# optional callback to stop training at a certain accuracy
+class myCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if(logs['accuracy'] > 0.998):
+            print("\nReached 99.8% accuracy so cancelling training!")
+            self.model.stop_training = True
+
+mnist = tf.keras.datasets.mnist
+(training_images, training_labels), (test_images, test_labels) = mnist.load_data()
+
+# reshaping data
+training_images = training_images.reshape(60000, 28, 28, 1)
+test_images = test_images.reshape(10000, 28, 28, 1)
+
+# normalizing data
+training_images = training_images/255
+test_images = test_images/255
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(32 , (3,3), activation='relu', input_shape=(28, 28, 1)),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation=tf.nn.relu), 
+    tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+callback = myCallback()
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.summary()
+model.fit(training_images, training_labels, epochs=20, callbacks=[callback])
+test_loss, test_accuracy = model.evaluate(test_images, test_labels)
+print("Accuracy: ", test_accuracy)
